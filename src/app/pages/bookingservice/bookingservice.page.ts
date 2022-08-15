7be86714573855d2cac8bb98d2490160f7c31282
Julian Voss <julian.voss@hs-osnabrueck.de>
2022-08-15 16:59:33 +0200

    booking process

--- src/app/pages/bookingservice/bookingservice.page.ts
+++ src/app/pages/bookingservice/bookingservice.page.ts
@@ -1,20 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild  } from '@angular/core';
+import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper';
+import { IonDatetime, IonicSlides, IonSlides } from '@ionic/angular';
 import { Router } from '@angular/router';
+
+SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom, IonicSlides]);
 
 @Component({
   selector: 'app-bookingservice',
   templateUrl: './bookingservice.page.html',
   styleUrls: ['./bookingservice.page.scss'],
 })
-export class BookingservicePage implements OnInit {
 
-  constructor(private router: Router) { }
+export class BookingservicePage {
+  @ViewChild('slides') slides: IonSlides;
+  @ViewChild('calendar') calendar: IonDatetime;
 
-  ngOnInit() {
-  }
+  currentSlide = 0;
+
+  constructor(
+    private router: Router,
+    ) { }
 
   abort() {
+    this.router.navigateByUrl('home', {replaceUrl: true});
+  }
+
+  back() {
+    this.currentSlide--;
+    this.slides.slidePrev();
+  }
+
+  chooseProviderSlide() {
+    this.currentSlide++;
+    this.slides.slideNext();
+  }
+
+  chooseDateSlide() {
+    this.currentSlide++;
+    this.slides.slideNext();
+  }
+
+  confirmSlide() {
+    this.currentSlide++;
+    this.slides.slideNext();
+  }
+
+  confirmBooking() {
     this.router.navigateByUrl('home', { replaceUrl: true });
   }
 
+  calendarChange($event) {
+    console.log(this.calendar.value);
+  }
 }
